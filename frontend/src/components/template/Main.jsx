d6f7bf879757d50ed9542604eb66776487418005
Leticia <misileco@hotmail.com>
2020-11-08 20:40:42 -0300

    first version

---
+++ frontend/src/components/template/Main.jsx
@@ -0,0 +1,15 @@
+import './Main.css';
+import React from 'react';
+import Header from './Header';
+
+export default props =>
+    <React.Fragment>
+        <Header {...props}/>
+        <main className="content container-fluid">
+            {/* p-3 mt-3 > padding 3 e  margintop de 3*/}
+            <div className="p-3 mt-3">
+                {props.children}
+            </div>
+        </main>
+
+    </React.Fragment>
